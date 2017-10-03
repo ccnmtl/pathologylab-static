113d3e2ab416dcb54dc26542f4d99b5cd1a67971
Nick Buonincontri <nbuonin@gmail.com>
2017-10-03 14:35:09 -0400

    Setup new theme and Travis CI integration

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "amd": true,
+        "jquery": true
+    },
+    "plugins": [
+        "security",
+        "scanjs-rules",
+        "no-unsafe-innerhtml"
+    ],
+    "extends": [
+        "eslint:recommended",
+        "plugin:security/recommended"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-unused-vars": [
+            "error",
+            {"vars": "all", "args": "none"}
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "security/detect-object-injection": "off",
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- Makefile
+++ Makefile
@@ -4,8 +4,7 @@
 #PROD_BUCKET=pathologylab.ctl.columbia.edu
 INTERMEDIATE_STEPS ?= make $(PUBLIC)/js/all.json
 
-JS_FILES=static/js/search.js static/js/srcswap.js static/js/alphalist.js static/js/bgswap.js \
-static/js/scrollshrink.js static/js/scrollspy.js static/js/widgets.js
+JS_FILES=themes/ctl-pathologylabs/static/js/
 
 all: eslint
 
